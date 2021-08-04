ba9be14a7f5068baee7a8f62cd514a070436d322
Rodrigo Marquez <rod.m89@gmail.com>
2021-08-03 20:05:10 -0400

    added log

--- ninjagold/urls.py
+++ ninjagold/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
   path('reset', views.reset),
   path('success', views.success),
   path('earn/<place>', views.earn),
-  # path('farm', views.farm),
-  # path('cave', views.cave),
-  # path('house', views.house),
-  # path('casino', views.casino),
 ]
--- ninjagold/views.py
+++ ninjagold/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 import random
+from datetime import datetime
 
 def index(request):
 
   if 'gold' not in request.session:
     request.session['gold'] = 0
+
+  if 'log' not in request.session:
+    request.session['log'] = []
   
   return render(request,'index.html')
 
@@ -32,33 +36,28 @@ def earn(request, place):
   rand = random.randint(miin,maax)
   
   request.session['gold'] += rand
+
+  request.session['color'] = 'white'
+
+  if rand >= 0:
+
+    request.session['log'].insert(0, f'Earned {rand} gold from {place} ({datetime.now().strftime("%Y-%m-%d %H:%M:%S")})')
+    request.session['color'] = 'success'
+
+  else:
+
+    request.session['log'].insert(0, f'Lost {-rand} gold from {place} ({datetime.now().strftime("%Y-%m-%d %H:%M:%S")})')
+    request.session['color'] = 'danger'
   
   return redirect ('/success')
 
 def reset(request):
   
   request.session['gold'] = 0
+  request.session['log'] = []
+
   return redirect('/success')
 
 def success(request):
+
   return render(request, 'play.html')
-
-# def farm(request):
-
-#   request.session['gold'] += random.randint(10,20)
-#   return redirect('/success')
-
-# def cave(request):
-
-#   request.session['gold'] += random.randint(5,10)
-#   return redirect('/success')
-
-# def house(request):
-
-#   request.session['gold'] += random.randint(2,5)
-#   return redirect('/success')
-
-# def casino(request):
-
-#   request.session['gold'] += random.randint(-50,50)
-#   return redirect('/success')
